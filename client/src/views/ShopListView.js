e80b98bcdb288316682fe94d96fa26043bcbd3ea
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-11 19:34:01 -0700

    Created ShopListView.js component

---
+++ client/src/views/ShopListView.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { appFirestore } from "../firebase/config";
+
+const ShopListView = () => {
+  return (
+    <div>
+      <div className="row">
+        <h1>Shopping List</h1>
+        <div className="col-md 8"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopListView;
