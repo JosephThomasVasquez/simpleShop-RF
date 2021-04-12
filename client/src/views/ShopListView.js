5384758f7b596b9747601c2f8fe26494fad5d032
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-12 14:53:15 -0700

    Added handleChnage function to handle onChange event of the add item input and also handleSubmit for the form

--- client/src/views/ShopListView.js
+++ client/src/views/ShopListView.js
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import FirestoreGetCollection from '../components/firebase/FirestoreGetCollection';
+import FirestoreGetCollection from "../components/firebase/FirestoreGetCollection";
 import data from "../data/data";
 
 const ShopListView = () => {
   const [shopItemsList, setShopItemsList] = useState([...data]);
-  const { firestoreDocs } = FirestoreGetCollection("shoppingLists")
+  const { firestoreDocs } = FirestoreGetCollection("shoppingLists");
   console.log(shopItemsList);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "item") {
+      setShopItemsList({ ...shopItemsList, item: value });
+    }
+  };
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+
+    console.log("items:", shopItemsList);
+    
+  };
 
   return (
     <div>
@@ -14,10 +28,15 @@ const ShopListView = () => {
         <h1 className="pt-5">Shopping List</h1>
         <Row>
           <Col md={8}>
-            <Form>
+            <Form onSubmit={handleAddItem}>
               <Form.Group controlId="addItemInput">
                 <Form.Label>Add Item to your list</Form.Label>
-                <Form.Control type="text" placeholder="Add item..." />
+                <Form.Control
+                  type="text"
+                  name="item"
+                  placeholder="Add item..."
+                  onChange={handleChange}
+                />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Add Item
