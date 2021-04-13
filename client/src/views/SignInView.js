b569e0e82cceb24607aa6199fbe143cb2fbba711
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-13 05:20:52 -0700

    Created SignInView view component

---
+++ client/src/views/SignInView.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+const SignInView = () => {
+  return (
+    <div>
+      <Container>
+        <h1 className="pt-5">Sign In</h1>
+        <Row>
+          <Col md={12}>
+            <Form onSubmit="">
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" name="email" placeholder="email" />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Oassword</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Sign In
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default SignInView;
