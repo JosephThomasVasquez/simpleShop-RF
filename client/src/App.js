8699936a94a79cef719d4c9c5c2a667e35e37b2c
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-14 06:34:45 -0700

    Added SignUpView to the routes of App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './bootstrap.min.css';
 import HomeView from "./views/HomeView";
 import SignInView from "./views/SignInView";
+import SignUpView from "./views/SignUpView";
 import ShopListView from "./views/ShopListView";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomeView} />
         <Route exact path="/signin" component={SignInView} />
+        <Route exact path="/signup" component={SignUpView} />
         <Route exact path="/shoppinglist" component={ShopListView} />
       </Switch>
     </Router>
