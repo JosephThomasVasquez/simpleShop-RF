41569664ce919e7b5628acd0d1c148dd75f26f4b
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-14 06:22:32 -0700

    Renamed navbar title to ShopList and removed search form from template

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">ShopList</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
@@ -17,14 +17,6 @@ const Header = () => {
               <NavDropdown.Item href="#action/3.2">Sign Out</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form inline>
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className="mr-sm-2"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
     </div>
