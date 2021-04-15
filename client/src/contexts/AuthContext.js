6c7cdb842e1ff8357e11514e60809e3fd2a9ea8a
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-15 06:25:07 -0700

    Added signup function in AuthContext for creating a user with email and password

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -3,12 +3,19 @@ import { auth } from "../firebase/config";
 
 const AuthContext = React.createContext();
 
+
+// Use the React context hook
 export const useAuth = () => {
   return useContext(AuthContext);
 };
 
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState("");
+
+  // Create a new user with email and password
+  const signUp = (email, password) => {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
 
   const authValue = { currentUser };
 
