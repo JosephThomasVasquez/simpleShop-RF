822f6803e2fc3ceb23ea612791393b24099333cc
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-17 12:34:31 -0700

    Added custom render for ComponentView and passing props if there is a currentUser from AuthContext

--- client/src/components/AuthRoute.js
+++ client/src/components/AuthRoute.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 const AuthRoute = ({ component: ComponentView, ...Components }) => {
-  return <Route>{...Components}</Route>;
+  const { currentUser } = useAuth();
+
+  return (
+    <Route>
+      {...Components}
+      render=
+      {(props) => {
+        currentUser ? <ComponentView {...props} /> : <Redirect tp="signin" />;
+      }}
+    </Route>
+  );
 };
 
 export default AuthRoute;
