d11d2888c6aea480fadfb077df1d8ddbd1397383
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-30 10:12:25 -0700

    Installed sass

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import { Container, Row, Col } from "react-bootstrap";
 
 const HomeView = () => {
   return (
     <div>
       <Header />
-      <h1>Home View</h1>
+      <Container className="pt-4">
+        <h1>Home View</h1>
+      </Container>
     </div>
   );
 };
