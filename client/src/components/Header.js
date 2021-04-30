36945eb412071eae6e9fc14ba2032990e76c091c
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-30 11:22:08 -0700

    Added fontawesome icon for the navbar Brand and imported google logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <div>
       <Navbar bg="primary" variant="dark" expand="lg">
-        <Navbar.Brand href="/">ShopList</Navbar.Brand>
+        <Navbar.Brand href="/"><i class="far fa-clipboard icon-brand"></i>ShopList</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
