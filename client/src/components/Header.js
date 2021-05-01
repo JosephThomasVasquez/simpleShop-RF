7aa90bb40b106c2b6fcfa3c9404227747115e072
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-01 11:25:26 -0700

    Added button around the user nav dropdown and added styling with shadows

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -14,32 +14,52 @@ const Header = () => {
 
   return (
     <div>
-      <Navbar bg="primary" variant="dark" expand="lg" className="gradient-header">
-        <Navbar.Brand href="/"><i className="far fa-clipboard icon-brand"></i><span className="ml-3 align-text-bottom brand-title">ShopList</span></Navbar.Brand>
+      <Navbar
+        bg="primary"
+        variant="dark"
+        expand="lg"
+        className="gradient-header"
+      >
+        <Navbar.Brand href="/">
+          <i className="far fa-clipboard icon-brand"></i>
+          <span className="ml-3 align-text-bottom brand-title">ShopList</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/" className="text-white">Home</Nav.Link>
-            <Nav.Link href="/shoppinglist" className="text-white">ShoppingList</Nav.Link>
+            <Nav.Link href="/" className="text-white">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/shoppinglist" className="text-white">
+              ShoppingList
+            </Nav.Link>
             {currentUser ? (
-              <NavDropdown
-                title={
-                  currentUser.displayName
-                    ? currentUser.displayName
-                    : currentUser.email
-                }
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="/signin">Sign In</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="/" onClick={handleSignOut}>
-                  Sign Out
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Button variant="primary" className="user-dropdown-btn gradient-buttons">
+                <NavDropdown
+                  title={
+                    currentUser.displayName
+                      ? currentUser.displayName
+                      : currentUser.email
+                  }
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item href="/signin">Sign In</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/" onClick={handleSignOut}>
+                    Sign Out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Button>
             ) : (
-              <Nav.Link href="signin" className="text-white">Sign In</Nav.Link>
+              <Nav.Link href="signin" className="text-white">
+                Sign In
+              </Nav.Link>
             )}
-            {!currentUser && <Nav.Link href="signup" className="text-white">Sign Up</Nav.Link>}
+            {!currentUser && (
+              <Nav.Link href="signup" className="text-white">
+                Sign Up
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
