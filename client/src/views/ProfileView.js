ae7c9333192497886d9b0385d7acc243c845ea10
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-04-30 20:06:01 -0700

    Added ProfileView component for user details

---
+++ client/src/views/ProfileView.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import { Container, Row, Col } from "react-bootstrap";
+
+const ProfileView = () => {
+  return (
+    <div>
+      <Header />
+      <Container className="pt-4">
+        <h1>Home View</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default ProfileView;
