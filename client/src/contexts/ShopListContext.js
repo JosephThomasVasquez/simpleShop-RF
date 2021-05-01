71e7de7e90bf7bc017f28790c9ed19aa3d2916d1
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-01 13:54:39 -0700

    Created shoplist context to hold state of shopping list

---
+++ client/src/contexts/ShopListContext.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useContext } from "react";
+
+const shopListContext = React.createContext();
+
+const ShopListProvider = () => {
+  return <div></div>;
+};
+
+export default ShopListProvider;
