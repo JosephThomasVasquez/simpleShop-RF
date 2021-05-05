011150e01f5a69b4c91bb124259256186379b900
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-04 20:19:36 -0700

    Added ErrorMessage component to handle displaying error messages

---
+++ client/src/components/ErrorMessage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Alert } from "react-bootstrap";
+
+const ErrorMessage = () => {
+  return <Alert variant="primary">There was an error!</Alert>;
+};
+
+export default ErrorMessage;
