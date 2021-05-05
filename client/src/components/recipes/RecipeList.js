e85334679338c96d9f5ef4093f21e4211c1d141e
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-05 06:41:41 -0700

    Added todos for RecipeList

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const RecipeList = () => {
+
+    // Create state hook for API data
+
+    // Fetch API with headers
+
+    // Update state with API data
+
     return (
         <div>
             <h2>Ey Yo!</h2>
