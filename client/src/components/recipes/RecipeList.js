020c9a8c1a3b353b1e05db3c037b4dc4c4ae6e1d
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-06 07:05:29 -0700

    Using fetch but still get 403 forbidden

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -4,38 +4,27 @@ import unirest from "unirest";
 const RecipeList = () => {
   const [recipesList, setRecipesList] = useState();
 
-  const getRecipes = () => {
-    const req = unirest("GET", "https://tasty.p.rapidapi.com/recipes/list");
-
-    req.query({ "from": "0"," size": "20", "tags": "under_30_minutes" });
-
-    req.headers({
-      "x-rapidapi-key": `${process.env.REACT_APP_TASTY_API_KEY}`,
-      "x-rapidapi-host": "tasty.p.rapidapi.com",
-      "useQueryString": true,
-      "credentials": "include",
-    });
-
-    req.end(function (res) {
-      if (res.error) throw new Error(res.error);
-
-      console.log(res.body);
-    });
-
-    // fetch("https://tasty.p.rapidapi.com/recipes/auto-complete", {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "x-rapidapi-key": "b2f5ac83famshe8f912d34765e81p13d57ajsn49a745a48b65",
-    //     "x-rapidapi-host": "tasty.p.rapidapi.com",
-    //     useQueryString: true,
-    //   },
-    // }).then((response) => {
-    //   console.log(response.json);
-    // });
+  const getRecipes = async () => {
+    fetch(
+      "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-rapidapi-key": `${process.env.REACT_APP_TASTY_API_KEY}`,
+          "x-rapidapi-host": "tasty.p.rapidapi.com",
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
-//   getRecipes();
+  getRecipes();
 
   // Get search query for API
 
