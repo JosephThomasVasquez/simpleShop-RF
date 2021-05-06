88ea942f7b24f046f222e5f29dce333ee1c3cbc8
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-05 20:48:37 -0700

    Added recipesList state

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const RecipeList = () => {
+  const [recipesList, setRecipesList] = useState();
 
-    // Create state hook for API data
+  // Create state hook for API data
 
-    // Get search query for API
+  // Get search query for API
 
-    // Fetch API with headers
+  // Fetch API with headers
 
-    // Update state with API data from query
+  // Update state with API data from query
 
-    return (
-        <div>
-            <h2>Ey Yo!</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Ey Yo!</h2>
+    </div>
+  );
+};
 
-export default RecipeList
+export default RecipeList;
