a1d49c35774f1f5cfca6b2314289024cad99d0cb
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-05 21:00:45 -0700

    Added getRecipes function to fetch recipes from Tasty API

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -3,6 +3,22 @@ import React, { useState } from "react";
 const RecipeList = () => {
   const [recipesList, setRecipesList] = useState();
 
+  const getRecipes = () => {
+    fetch("https://tasty.p.rapidapi.com/recipes/auto-complete", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-rapidapi-key": "b2f5ac83famshe8f912d34765e81p13d57ajsn49a745a48b65",
+        "x-rapidapi-host": "tasty.p.rapidapi.com",
+        useQueryString: true,
+      },
+    }).then((response) => {
+      console.log(response.json);
+    });
+  };
+
+  getRecipes();
+
   // Create state hook for API data
 
   // Get search query for API
