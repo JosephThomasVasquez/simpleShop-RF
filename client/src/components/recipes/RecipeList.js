a8423bc0a8aaf2a4962e2d50f497ee92eb25cc4d
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-06 21:34:02 -0700

    Added react-bootstrap grid components

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import unirest from "unirest";
 
 const RecipeList = ({ searchRecipes }) => {
-  // getRecipes();
-
-  // Get search query for API
-
-  // Fetch API with headers
-
-  // Update state with API data from query
-
   return (
-    <div>
+    <Container>
       <h2>{searchRecipes.length > 0 ? "Search Results:" : "Ey Yo!"}</h2>
-      <div>{searchRecipes && JSON.stringify(searchRecipes)}</div>
-    </div>
+      <Row>
+        <Col>
+          <div>{searchRecipes && JSON.stringify(searchRecipes)}</div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
