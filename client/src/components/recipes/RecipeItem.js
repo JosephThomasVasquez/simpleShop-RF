8beb6379ca01662b2330eb05c63acc2295f9eeb0
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-07 12:53:49 -0700

    Added recipe diet and calories to each recipe item

--- client/src/components/recipes/RecipeItem.js
+++ client/src/components/recipes/RecipeItem.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Row, Col, Button, Card } from "react-bootstrap";
 const RecipeItem = ({ recipe }) => {
-
   return (
     <Col md={4}>
       <Card className="shadow m-2">
         <Card.Img variant="top" src={recipe.image} />
         <Card.Body>
           <Card.Title>{recipe.title}</Card.Title>
-          <Card.Text>Spoonacular API</Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Card.Text>Cal. {Math.floor(recipe.calories)}</Card.Text>
+          {recipe.dietLabels.length > 0 ? (
+            <Card.Text>Diet: {recipe.dietLabels}</Card.Text>
+          ) : (
+            <Card.Text>{' '}</Card.Text>
+          )}
+          <Button variant="secondary" className="gradient-buttons">View Recipe</Button>
         </Card.Body>
       </Card>
     </Col>
