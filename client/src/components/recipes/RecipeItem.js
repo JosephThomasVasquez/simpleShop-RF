c085225a0f1c785ddca890f27cbf909659a8b958
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-06 21:58:03 -0700

    Added card styling from react bootstrap to the recipe items

--- client/src/components/recipes/RecipeItem.js
+++ client/src/components/recipes/RecipeItem.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Col, Button, Card } from "react-bootstrap";
-const RecipeItem = ({recipe}) => {
-
-    console.log(recipe);
+const RecipeItem = ({ recipe }) => {
+  console.log(recipe);
 
   return (
     <Col>
       <Card></Card>
-      <div>{JSON.stringify(recipe)}</div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={recipe.image} />
+        <Card.Body>
+          <Card.Title>{recipe.title}</Card.Title>
+          <Card.Text>Spoonacular API</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </Col>
   );
 };
