eacb61f68c167ab43eff6278d2ecd0917d9d57e1
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-07 21:28:31 -0700

    Added RecentShoppingLists componente

---
+++ client/src/components/shopList/RecentShoppingLists.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+const RecentShoppingLists = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h4>Recent ShoppingLists</h4>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default RecentShoppingLists;
