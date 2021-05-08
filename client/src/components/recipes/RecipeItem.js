ed7baaf0088540d90a4a41d31a058eb9a9f2b29f
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-07 21:45:57 -0700

    Adjusted styling on recipe item cards

--- client/src/components/recipes/RecipeItem.js
+++ client/src/components/recipes/RecipeItem.js
@@ -7,8 +7,8 @@ const RecipeItem = ({ recipe }) => {
     <Col md={4}>
       <Card className="shadow m-2">
         <Card.Img className="recipe-item-img" variant="top" src={recipe.image} />
-        <Card.Body>
-          <Card.Title>{recipe.label}</Card.Title>
+        <Card.Body className="p-2">
+          <Card.Title className="m-0">{recipe.label}</Card.Title>
           <Card.Text className="m-0">Cal. {Math.floor(recipe.calories)}</Card.Text>
           {recipe.dietLabels.length > 0 ? (
             <Card.Text className="m-0 mb-2">Diet: {recipe.dietLabels}</Card.Text>
