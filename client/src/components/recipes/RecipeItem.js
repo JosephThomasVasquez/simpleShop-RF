d61154821bc5013665eea945d8e81d612d6c6c41
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-16 16:12:10 -0700

    Updated firebase cli hosting by running init in root folder

--- client/src/components/recipes/RecipeItem.js
+++ client/src/components/recipes/RecipeItem.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Row, Col, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import RecipesDetailsView from "../../views/RecipeDetailsView";
 
 const RecipeItem = ({ recipe }) => {
   return (
     <Col md={6} lg={4} xl={4}>
       <Card className="shadow m-2">
-        <Card.Img className="recipe-item-img" variant="top" src={recipe.image} />
+        <Card.Img
+          className="recipe-item-img"
+          variant="top"
+          src={recipe.image}
+        />
         <Card.Body className="p-2">
           <Card.Title className="m-0">{recipe.label}</Card.Title>
-          <Card.Text className="m-0">Cal. {Math.floor(recipe.calories)}</Card.Text>
+          <Card.Text className="m-0">
+            Cal. {Math.floor(recipe.calories)}
+          </Card.Text>
           {recipe.dietLabels.length > 0 ? (
-            <Card.Text className="m-0 mb-2">Diet: {recipe.dietLabels}</Card.Text>
+            <Card.Text className="m-0 mb-2">
+              Diet: {recipe.dietLabels}
+            </Card.Text>
           ) : (
             <Card.Text> </Card.Text>
           )}
