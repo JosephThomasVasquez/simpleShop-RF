34e561e3cc366a8a71b375cebd972080413c5695
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-19 21:17:09 -0700

    Switch routes to recipe details view with location recipe state

--- src/views/RecipeDetailsView.js
+++ src/views/RecipeDetailsView.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-const RecipeDetailsView = () => {
-  return <div></div>;
+const RecipeDetailsView = ({ location }) => {
+  const recipe = location.state.recipe;
+
+  console.log('recipe Data', recipe);
+
+  return (
+    <Container>
+      <h1>Recipe Details</h1>
+      <Row>
+        <Col md={12} sm={12}></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default RecipeDetailsView;
