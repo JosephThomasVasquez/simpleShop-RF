440dbfccb258c34bd244a3bdfabc12b8967185f5
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-19 22:02:06 -0700

    Adjusted styling of recipe elements

--- src/views/RecipeDetailsView.js
+++ src/views/RecipeDetailsView.js
@@ -4,16 +4,43 @@ import { Container, Row, Col } from "react-bootstrap";
 const RecipeDetailsView = ({ location }) => {
   const recipe = location.state.recipe;
 
+  const {
+    label,
+    calories,
+    cuisineType,
+    dietLabels,
+    digest,
+    dishType,
+    healthLabels,
+    image,
+    ingredients,
+  } = recipe;
+
   useEffect(() => {
     console.log("location", location);
     console.log("recipe Data", recipe);
   }, [location]);
 
   return (
-    <Container>
-      <h1>Recipe Details</h1>
+    <Container className="bg-white mt-4 p-4 pb-5 shadow">
+      <h1 className="text-primary">{label}</h1>
+      <Row className="pb-4">
+        <Col lg={2} md={2} sm={2}>
+          {dishType}
+        </Col>
+        <Col lg={2} md={2} sm={2} className="text-primary">
+          Calories: {calories}
+        </Col>
+      </Row>
       <Row>
-        <Col md={12} sm={12}></Col>
+        <Col lg={6} md={6} sm={12}>
+          <img src={image} alt={label} className="shadow rounded" />
+        </Col>
+        <Col lg={6} md={6} sm={12}>
+          <ul>
+            {ingredients && ingredients.map((item) => <li>{item.text}</li>)}
+          </ul>
+        </Col>
       </Row>
     </Container>
   );
