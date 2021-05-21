8f368f3a7226a01ac381a98977cdadebbef80a5d
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-21 08:35:55 -0700

    Added basic forms and bootstrap components

--- src/views/ForgotPasswordView.js
+++ src/views/ForgotPasswordView.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import { signInWithGoogle } from "../firebase/config";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import ErrorMessage from "../components/ErrorMessage";
+import googleLogo from "../icons/Google__G__Logo.svg.png";
 
 const ForgotPasswordView = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const emailRef = useRef();
 
-export default ForgotPasswordView
+  const [emailError, setEmailError] = useState("");
+
+  const { signIn } = useAuth();
+  const history = useHistory();
+
+  const handleForgotPasswordSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setEmailError("");
+    } catch (error) {
+      setEmailError("Invalid email address. Please check email.");
+      console.log("Email Error", emailError);
+    }
+  };
+
+  return (
+    <div>
+      <Container className="py-4 my-4 bg-white shadow">
+        <Row>
+          <Col>
+            <h1>Forgot Password</h1>
+          </Col>
+        </Row>
+        {emailError && (
+          <Row>
+            <Col>
+              <ErrorMessage errorMsg={emailError} />
+            </Col>
+          </Row>
+        )}
+
+        <Row className="mt-5">
+          <Col md={12}>
+            <Form onSubmit={handleForgotPasswordSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  ref={emailRef}
+                />
+              </Form.Group>
+
+              <Row className="ml-auto">
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="gradient-buttons"
+                >
+                  Sign In
+                </Button>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default ForgotPasswordView;
